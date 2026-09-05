1dad138063e1d7c5369ec8b6a4021904bf0f5aa5
freakyyirus <freakyyirus@users.noreply.github.com>
2026-09-05 10:46:39 +0530

    Add shared component: index.ts

---
+++ frontend/src/components/shared/index.ts
@@ -0,0 +1,13 @@
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as Input } from './Input';
+export { default as Select } from './Select';
+export { default as Modal } from './Modal';
+export { default as Toast, useToastStore } from './Toast';
+export { default as LoadingSpinner } from './LoadingSpinner';
+export { default as Sidebar } from './Sidebar';
+export { default as Header } from './Header';
+export { default as ProtectedRoute } from './ProtectedRoute';
+export { default as Badge } from './Badge';
+export { default as EmptyState } from './EmptyState';
+export { default as StatsCard } from './StatsCard';
